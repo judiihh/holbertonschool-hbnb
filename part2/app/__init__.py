15337a3395049a2463aaa54973fd38d86bc25ea0
Judith <j.espinalm12@gmail.com>
2025-03-02 23:15:10 -0400

    Edited functions python file from api

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restx import Api
+from api.v1.users import api as users_ns
 
 def create_app():
     """Initialize Flask app and set up API."""
     app = Flask(__name__)
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
-
-    # API placeholders (endpoints will be added later)
+    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
+    
+    # Register the users namespace
+    api.add_namespace(users_ns, path='/api/v1/users')
 
     return app
