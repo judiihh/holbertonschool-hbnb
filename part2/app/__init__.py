aede8939ac3c45512b1d9933b4fe909bd9e8971a
Judith <j.espinalm12@gmail.com>
2025-03-02 23:23:35 -0400

    Edited python init file from app

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -6,7 +6,7 @@ def create_app():
     """Initialize Flask app and set up API."""
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
-    
+
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
 
