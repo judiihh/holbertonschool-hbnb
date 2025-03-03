bc6c6925698f2b0111293f5987a47437df136f89
Judith <j.espinalm12@gmail.com>
2025-03-02 23:26:56 -0400

    Edited file

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restx import Api
-from api.v1.users import api as users_ns
+from app.api.v1.users import api as users_ns
 
 def create_app():
     """Initialize Flask app and set up API."""
