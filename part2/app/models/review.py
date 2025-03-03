5c5cc66ac4591e0920b9248f0265318a49692f61
Judith <j.espinalm12@gmail.com>
2025-03-02 22:34:45 -0400

    Added review file

---
+++ part2/app/models/review.py
@@ -0,0 +1,11 @@
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """Review model linked to a Place"""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes a Review instance"""
+        super().__init__(*args, **kwargs)
+        self.place_id = ""
+        self.user_id = ""
+        self.text = ""
