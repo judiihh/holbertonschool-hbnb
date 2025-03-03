5caeca259703359962bdcdb003d950d8358c19c7
Judith <j.espinalm12@gmail.com>
2025-03-02 22:35:03 -0400

    Added user file

---
+++ part2/app/models/user.py
@@ -0,0 +1,12 @@
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """User model with authentication attributes"""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes a User instance"""
+        super().__init__(*args, **kwargs)
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
