f2c1622acd90ab3da3b7e6851eeb1b41e4578e8a
Judith <j.espinalm12@gmail.com>
2025-03-03 20:29:34 -0400

    Added repository file in persistence

---
+++ part2/app/persistence/repository.py
@@ -0,0 +1,9 @@
+class InMemoryRepository:
+    def __init__(self):
+        self.data = {}
+
+    def add(self, obj):
+        self.data[obj.id] = obj
+
+    def get(self, obj_id):
+        return self.data.get(obj_id)
