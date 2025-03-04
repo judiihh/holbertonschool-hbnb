643d374517bfef0985fb1d8d6a94a785091f639b
Judith <j.espinalm12@gmail.com>
2025-03-03 20:13:44 -0400

    Edited facade for amenities

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,34 +1,32 @@
-from app.models.user import User
-from app.services.repository import InMemoryRepository
+from app.models.amenity import Amenity
+from app.persistence.repository import InMemoryRepository
 
 class HBnBFacade:
     def __init__(self):
-        self.user_repo = InMemoryRepository()
+        self.amenity_repo = InMemoryRepository()
 
-    def create_user(self, user_data):
-        """Creates a new user"""
-        user = User(**user_data)
-        self.user_repo.add(user)
-        return user
+    def create_amenity(self, amenity_data):
+        """Creates a new Amenity and stores it in the repository"""
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
 
-    def get_user(self, user_id):
-        """Fetch a user by ID"""
-        return self.user_repo.get(user_id)
+    def get_amenity(self, amenity_id):
+        """Retrieves an Amenity by its ID"""
+        return self.amenity_repo.get(amenity_id)
 
-    def get_user_by_email(self, email):
-        """Fetch a user by email"""
-        return self.user_repo.get_by_attribute('email', email)
+    def get_all_amenities(self):
+        """Retrieves all amenities"""
+        return self.amenity_repo.get_all()
 
-    def get_all_users(self):
-        """Returns a list of all users"""
-        return self.user_repo.get_all()
-
-    def update_user(self, user_id, updated_data):
-        """Updates a user"""
-        user = self.get_user(user_id)
-        if not user:
+    def update_amenity(self, amenity_id, amenity_data):
+        """Updates an existing Amenity's details"""
+        amenity = self.amenity_repo.get(amenity_id)
+        if not amenity:
             return None
-        for key, value in updated_data.items():
-            if hasattr(user, key):
-                setattr(user, key, value)
-        return user
+
+        for key, value in amenity_data.items():
+            setattr(amenity, key, value)
+
+        self.amenity_repo.update(amenity)
+        return amenity
