8f16b2c334cecd086a51a90f0ff8af7b32ffdf86
Judith <j.espinalm12@gmail.com>
2025-03-03 20:44:19 -0400

    Corrected output for repository in persistence

--- part2/app/persistence/repository.py
+++ part2/app/persistence/repository.py
@@ -1,9 +1,21 @@
 class InMemoryRepository:
+    """A simple in-memory repository to store entities"""
     def __init__(self):
-        self.data = {}
+        self.storage = {}
 
-    def add(self, obj):
-        self.data[obj.id] = obj
+    def add(self, entity):
+        """Adds an entity to the storage"""
+        self.storage[entity.id] = entity
 
-    def get(self, obj_id):
-        return self.data.get(obj_id)
+    def get(self, entity_id):
+        """Retrieves an entity by its ID"""
+        return self.storage.get(entity_id)
+
+    def update(self, entity):
+        """Updates an existing entity in the storage"""
+        if entity.id in self.storage:
+            self.storage[entity.id] = entity
+
+    def get_all(self):
+        """Retrieves all entities in the storage"""
+        return list(self.storage.values())  # Fix: Convert dict values to a list
