8bc4bc59d8ad8e6412f5882cc6be872a97ba2be5
Judith <j.espinalm12@gmail.com>
2025-03-03 22:08:25 -0400

    Modified place in models

--- part2/app/models/place.py
+++ part2/app/models/place.py
@@ -4,15 +4,17 @@ class Place(BaseModel):
     """Represents a place (listing)"""
 
     def __init__(self, *args, **kwargs):
-        """Initializes a Place instance"""
+        """Initializes a Place instance with given attributes"""
         super().__init__(*args, **kwargs)
-        self.user_id = ""
-        self.name = ""
-        self.description = ""
-        self.number_rooms = 0
-        self.number_bathrooms = 0
-        self.max_guest = 0
-        self.price_by_night = 0
-        self.latitude = 0.0
-        self.longitude = 0.0
-        self.amenity_ids = []
+
+        # Default values
+        self.user_id = kwargs.get("user_id", "")
+        self.name = kwargs.get("name", "")
+        self.description = kwargs.get("description", "")
+        self.number_rooms = kwargs.get("number_rooms", 0)
+        self.number_bathrooms = kwargs.get("number_bathrooms", 0)
+        self.max_guest = kwargs.get("max_guest", 0)
+        self.price_by_night = kwargs.get("price_by_night", 0)
+        self.latitude = kwargs.get("latitude", 0.0)
+        self.longitude = kwargs.get("longitude", 0.0)
+        self.amenity_ids = kwargs.get("amenity_ids", [])
