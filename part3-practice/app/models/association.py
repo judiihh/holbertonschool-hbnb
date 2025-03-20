13445bca1d34b515b9f854b3b1e1a287feb75626
Judith Espinal <j.espinalm12@gmail.com>
2025-03-20 03:05:44 -0400

    Delete part3-practice directory

--- part3-practice/app/models/association.py
+++
@@ -1,6 +0,0 @@
-from app import db
-
-place_amenity = db.Table('place_amenity',
-    db.Column('place_id', db.Integer, db.ForeignKey('places.id'), primary_key=True),
-    db.Column('amenity_id', db.Integer, db.ForeignKey('amenities.id'), primary_key=True)
-)
